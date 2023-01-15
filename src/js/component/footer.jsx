41f3c645c1881aff2922a4f402b90237055bde5c
noedavico <noedavico@hotmail.com>
2023-01-15 14:51:29 +0000

    ajustes

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () =>{
     return(
-        <div className="row bg-dark" style={{margin: "10px"}}>
+        <div className="row bg-dark fixed-bottom" style={{margin: "10px"}}>
         <div className="col-lg-12 text-white text-center">
             <p>Copyright &copy; Your Website Name 2021</p>
         </div>
